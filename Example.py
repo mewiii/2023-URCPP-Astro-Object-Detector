0d6e4a0e75a2452e290a53e8b51533baecd424b6
Deanna Wilborne <mewilbor@ncsu.edu>
2023-07-07 09:48:16 -0400

    Add files via upload
    Simple example for using the classes within ObjectCounter.py

---
+++ Example.py
@@ -0,0 +1,6 @@
+# example use of Object Counter
+
+from ObjectCounter import PrecisionObjectDetector
+pod = PrecisionObjectDetector("./images/44x29-test-2011-05Andreo_BigDipper7k.png")
+print(pod.object_count)
+
